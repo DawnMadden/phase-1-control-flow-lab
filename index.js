0e983e26abe8f43890751b33690253d6b5e835ec
Dawn Madden <coachdawnmadden@gmail.com>
2023-09-04 15:33:50 -0400

    done

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+if (ride <= 400) { 
+  return 'This one is on me!';
+}else if (ride > 400 && ride <= 2000){
+    return 'That will be twenty bucks.';
+}else if (ride > 2000 && ride < 2500){
+    return 'I will gladly take your thirty bucks.';
+}else if (ride > 2500){
+  return 'No can do.';
+}
+}
+function ternaryCheckCity(destination){
+  // Write your code here!
+  return destination === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tipping){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+switch (tipping){
+  case 'generous':
+      return 'Thank you so much.'
+  case 'not as generous':
+      return 'Thank you.'
+  case 'thanks for everything':
+      return 'Bye.'
+} 
 }
